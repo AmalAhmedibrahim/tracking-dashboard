b8a6e2c9a48d601e0d287e0b433588074f25c169
Amal Ahmed <dev.amal.ahmed@gmail.com>
2023-12-17 22:16:28 +0200

    update state card styling

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardContainerComponent } from './containers/dashboard-container/dashboard-container.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CardsModule } from '../../shared/cards/cards.module';
 import { ProductsService } from 'src/app/services/products/products.service';
 
 @NgModule({
-  declarations: [DashboardContainerComponent, DashboardComponent],
+  declarations: [DashboardContainerComponent],
   providers: [ProductsService],
   imports: [CommonModule, DashboardRoutingModule, CardsModule],
 })
